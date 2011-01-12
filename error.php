ac3654a8f8f3f5b0ffcc1265660451ec15d30a65
Dallas Gutauckis <dgutauckis@myyearbook.com>
2011-01-11 23:28:16 -0500

    Adding comments to error file

--- error.php
+++ error.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * A file for handling errors with invalid types
+ *
+ * @author Dallas Gutauckis <dallas@myyearbook.com>
+ * @since 2011-01-12 23:27:00 EST
+ */
+
 function handleERROR()
 {
   echo 'ERROR: INVALID TYPE';
